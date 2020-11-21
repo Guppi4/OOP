d6559c8c35c6afa1d50f05d40b74dd8de64f11a4
Guppi4 <kirill.perevalov.1996@gmail.com>
2020-11-21 02:10:33 +0200

    //copy

--- src/ex1/tests/WGraph_AlgoTest.java
+++ src/ex1/tests/WGraph_AlgoTest.java
@@ -1,17 +1,18 @@
 package ex1.tests;
 
+import ex1.src.*;
 import org.junit.jupiter.api.Test;
-import ex1.src.WGraph_Algo;
-import ex1.src.node_info;
-import ex1.src.weighted_graph;
-import ex1.src.weighted_graph_algorithms;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class WGraph_AlgoTest {
 
+    private static Random _rnd = null;
     @Test
     void isConnected() {
         weighted_graph g0 = WGraph_DSTest.graph_creator(0,0,1);
@@ -79,6 +80,15 @@ class WGraph_AlgoTest {
         g0.removeNode(0);
         assertNotEquals(g0,g1);
     }
+    @Test
+    void copy(){
+        weighted_graph g=graph_creator(10,10,1);
+        weighted_graph_algorithms gal=new WGraph_Algo() ;
+        gal.init(g);
+        weighted_graph g1=gal.copy();
+        assertEquals(g,g1);
+
+    }
 
     private weighted_graph small_graph() {
         weighted_graph g0 = WGraph_DSTest.graph_creator(11,0,1);
@@ -102,4 +112,51 @@ class WGraph_AlgoTest {
 
         return g0;
     }
+
+    public static weighted_graph graph_creator(int v_size, int e_size, int seed) {
+        weighted_graph g = new WGraph_DS();
+        _rnd = new Random(seed);
+        for(int i=0;i<v_size;i++) {
+            g.addNode(i);
+        }
+        // Iterator<node_data> itr = V.iterator(); // Iterator is a more elegant and generic way, but KIS is more important
+        int[] nodes = nodes(g);
+        while(g.edgeSize() < e_size) {
+            int a = nextRnd(0,v_size);
+            int b = nextRnd(0,v_size);
+            int i = nodes[a];
+            int j = nodes[b];
+            double w = _rnd.nextDouble();
+            g.connect(i,j, w);
+        }
+        return g;
+    }
+    private static int nextRnd(int min, int max) {
+        double v = nextRnd(0.0+min, (double)max);
+        int ans = (int)v;
+        return ans;
+    }
+    private static double nextRnd(double min, double max) {
+        double d = _rnd.nextDouble();
+        double dx = max-min;
+        double ans = d*dx+min;
+        return ans;
+    }
+    /**
+     * Simple method for returning an array with all the node_data of the graph,
+     * Note: this should be using an Iterator<node_edge> to be fixed in Ex1
+     * @param g
+     * @return
+     */
+    private static int[] nodes(weighted_graph g) {
+        int size = g.nodeSize();
+        Collection<node_info> V = g.getV();
+        node_info[] nodes = new node_info[size];
+        V.toArray(nodes); // O(n) operation
+        int[] ans = new int[size];
+        for(int i=0;i<size;i++) {ans[i] = nodes[i].getKey();}
+        Arrays.sort(ans);
+        return ans;
+    }
+
 }
